5a68e1d942ac453f9c3454366eff1fd3e88c37cf
tobeidat <toqaobeidat1@gmail.com>
2024-03-16 10:40:52 +0300

    newrepository

--- src/almosaferNewTestCases/myTestCases.java
+++ src/almosaferNewTestCases/myTestCases.java
@@ -22,66 +22,66 @@ public class myTestCases extends parameters {
 	public void setup() {
 		driver.manage().window().maximize();
 		driver.get(urls[index]);
-		
-		WebElement welcomeScreen=driver.findElement(By.className("cta__saudi"));
+
+		WebElement welcomeScreen = driver.findElement(By.className("cta__saudi"));
 		welcomeScreen.click();
 	}
+
 	@Test()
 	public void myTest() throws InterruptedException, IOException {
-		WebElement hotelTab=driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		WebElement hotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
 		hotelTab.click();
 		Thread.sleep(2000);
-		
-		
-		if(driver.getCurrentUrl().contains("en"))
-		{	WebElement hotles=driver.findElement(By.cssSelector("input[placeholder='Search for hotels or places']"));
+
+		if (driver.getCurrentUrl().contains("en")) {
+			WebElement hotles = driver.findElement(By.cssSelector("input[placeholder='Search for hotels or places']"));
 
 			hotles.sendKeys(englishCities[randomIndex]);
-		}
-		else
-		{
-			WebElement hotles=driver.findElement(By.cssSelector("input[placeholder='البحث عن فنادق أو وجهات']"));
+		} else {
+			WebElement hotles = driver.findElement(By.cssSelector("input[placeholder='البحث عن فنادق أو وجهات']"));
 
 			hotles.sendKeys(arabicCities[randomIndex1]);
 		}
-		WebElement reservationSelectSelectElement=driver.findElement(By.className("tln3e3-1"));
-		Select select =new Select(reservationSelectSelectElement);
-		int num=rand.nextInt(2);
+		WebElement reservationSelectSelectElement = driver.findElement(By.className("tln3e3-1"));
+		Select select = new Select(reservationSelectSelectElement);
+		int num = rand.nextInt(2);
 		select.selectByIndex(num);
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		
-		WebElement citiesList=driver.findElement(By.className("phbroq-4"));
-		List<WebElement>allCities=citiesList.findElements(By.tagName("li"));
-		allCities.get(1).click();
-		
-		WebElement HotelSearchBox=driver.findElement(By.className("sc-1vkdpp9-6"));
-		HotelSearchBox.click();
-		
-		Thread.sleep(15000);
-		
-		Date currentDate=new Date();
-		String currentDateAsString=currentDate.toString().replace(":", "-");
-		
-		TakesScreenshot src= ((TakesScreenshot)driver);
-		
-		File srcFile= src.getScreenshotAs(OutputType.FILE);
-		
-        File Dest = new File("src/screenshot"+currentDateAsString+".png");
-		
-		FileUtils.copyFile (srcFile,Dest);
-		
-		WebElement hotelsSection=driver.findElement(By.className("col-9"));
 
-        List<WebElement> allPrices=hotelsSection.findElements(By.className("Price__Value"));
-        int firstItems= Integer.parseInt(allPrices.get(0).getText());
-        int lastItems= Integer.parseInt( allPrices.get(allPrices.size()-1).getText());
-        
-        Assert.assertEquals(firstItems > lastItems, true);
-		
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+		WebElement citiesList = driver.findElement(By.className("phbroq-4"));
+		List<WebElement> allCities = citiesList.findElements(By.tagName("li"));
+		allCities.get(1).click();
+
+		WebElement HotelSearchBox = driver.findElement(By.className("sc-1vkdpp9-6"));
+		HotelSearchBox.click();
+
+		Thread.sleep(15000);
+
+		Date currentDate = new Date();
+		String currentDateAsString = currentDate.toString().replace(":", "-");
+
+		TakesScreenshot src = ((TakesScreenshot) driver);
+
+		File srcFile = src.getScreenshotAs(OutputType.FILE);
+
+		File Dest = new File("src/screenshot" + currentDateAsString + ".png");
+
+		FileUtils.copyFile(srcFile, Dest);
+
+		WebElement hotelsSection = driver.findElement(By.className("col-9"));
+
+		List<WebElement> allPrices = hotelsSection.findElements(By.className("Price__Value"));
+
+		double firstItems = Double.parseDouble(allPrices.get(0).getText().replace(",", ""));
+		double lastItems = Double.parseDouble(allPrices.get(allPrices.size() - 1).getText().replace(",", ""));
+
+		Assert.assertEquals(firstItems > lastItems, true);
+
 	}
+
 	@AfterTest
-	public void postTest() {}
+	public void postTest() {
+	}
 
 }
--- src/almosaferNewTestCases/parameters.java
+++ src/almosaferNewTestCases/parameters.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class parameters {
-	WebDriver driver=new ChromeDriver();
-	
-	String [] urls= {"https://www.almosafer.com/ar","https://www.almosafer.com/en"};
-	String [] englishCities= {"jeddah","muscat"};
-	String [] arabicCities= {"دبي","الرياض"};
-	
+	WebDriver driver = new ChromeDriver();
+
+	String[] urls = { "https://www.almosafer.com/ar", "https://www.almosafer.com/en" };
+	String[] englishCities = { "jeddah", "muscat" };
+	String[] arabicCities = { "دبي", "الرياض" };
+
 	Random rand = new Random();
-	int index=rand.nextInt(urls.length);
-	int randomIndex=rand.nextInt(englishCities.length);
-	int randomIndex1=rand.nextInt(arabicCities.length);
+	int index = rand.nextInt(urls.length);
+	int randomIndex = rand.nextInt(englishCities.length);
+	int randomIndex1 = rand.nextInt(arabicCities.length);
 
 }
